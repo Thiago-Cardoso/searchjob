c3f8d994f37665def8c8e7a0c752e3cabc210877
bercht <bercht@live.com>
2018-07-30 18:35:47 -0300

    ajustes na home, controller Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root 'home#index'
+  get 'pages/home'
+  get 'pages/about'
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
