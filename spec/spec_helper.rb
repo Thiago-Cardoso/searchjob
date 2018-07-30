fa2a8427fe3285361842af2ce0fbd1e28e2968b4
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-30 19:32:41 -0300

    create get_file_as_string method in /spec/spec_helper.rb to open and read the html raw file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,12 +93,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-  def get_file_as_string(filename)
-    data = ''
-    f = File.open(filename, "r") 
-    f.each_line do |line|
-      data += line
-    end
-    return data
-  end
+
+
 end
