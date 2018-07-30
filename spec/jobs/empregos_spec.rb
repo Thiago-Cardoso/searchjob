89e42ff8503de1669d36864ffe7db676eb54327a
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-30 18:48:34 -0300

    delete mocks images folder, because is not necessary to this test

--- spec/jobs/empregos_spec.rb
+++ spec/jobs/empregos_spec.rb
@@ -1,6 +1,8 @@
-require_relative 'rails_helper'
-require_relative '/mocks/'
+require 'rails_helper'
 
 describe "job test of empregos.com.br with query 'programador ruby'" do
-
+    page = get_file_as_string("spec/mocks/Empregos.html")
+    #The variable page have the html content from Empregos.com.br 
+    #from query 'programador ruby'
+    
 end
