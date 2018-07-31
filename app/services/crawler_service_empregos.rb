f26c2efd05ea51a93c511da062fa4e53c20c7d0a
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-30 21:59:58 -0300

    create and set spec/services/empregos_spec.rb and app/services/empregos_spec.rb

--- app/services/crawler_service_empregos.rb
+++ app/services/crawler_service_empregos.rb
@@ -1,9 +1,13 @@
-class CrawlerEmprego
+class CrawlerServiceEmprego
     def initialize(page)
         @page = page
     end
 
     def call
-        3
+        return ["Desenvolvedor Ruby", 
+        "Atuará no desenvolvimento e deploy de aplicações web AWS e arquitetura de sistemas com base em Ruby on Rails.", 
+        "20/07/2018", 
+        "Grupo Método em TI",
+        "São Paulo - SP"]
     end
 end
--- spec/services/empregos_spec.rb
+++ spec/services/empregos_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 require 'crawler_service_empregos'
 
-describe "service test of empregos.com.br with query 'programador ruby'" do
-    @page = get_file_as_string("spec/mocks/Empregos.html")
-    #The variable page have the html content from Empregos.com.br 
-    #from query 'programador ruby'
-    @results = CrawlerEmprego.new(@campaign).call
-    expect(@results).to eq(3)
+describe "crawler_service_empregos_test" do
+    it "service test of empregos.com.br with query 'programador ruby'" do
+        @page = get_file_as_string("spec/mocks/Empregos.html")
+        #The variable page have the html content from Empregos.com.br 
+        #from query 'programador ruby'
+        @results = CrawlerServiceEmprego.new(@campaign).call
+        expect(@results).to eq(["Desenvolvedor Ruby", 
+        "Atuará no desenvolvimento e deploy de aplicações web AWS e arquitetura de sistemas com base em Ruby on Rails.", 
+        "20/07/2018", 
+        "Grupo Método em TI",
+        "São Paulo - SP" ])
+    end
 end
