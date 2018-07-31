bab435741f7f4d80d064a4b53ab4ab00b77d88c6
bercht <bercht@live.com>
2018-07-31 10:52:47 -0300

    ajustes na show jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,12 +1,15 @@
 class JobsController < ApplicationController
-	
+	before_action :set_job, only: :show
 	def index
 		@jobs = Job.all
 	end
-
+	def show
+	end
 
 
 	private
 	
-
+		def set_job
+			@job = Job.find(params[:id])
+		end
 end
