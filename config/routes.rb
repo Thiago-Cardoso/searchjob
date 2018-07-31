c8e0d92084569ebb275112e3a9d524cea9a781e0
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-31 00:36:44 -0300

    update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 require 'sidekiq/web'
 
 
-Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'pages/home'
-  get 'pages/about'
-  root 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-  
+Rails.application.routes.draw do 
   mount Sidekiq::Web => '/sidekiq'
->>>>>>> sidekiq
 end
