1cf0b7cfcd077dfcd937682ccc88d45e74645c45
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-31 12:47:56 -0300

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 require 'sidekiq/web'
-<<<<<<< HEAD
-
-
-Rails.application.routes.draw do 
-  mount Sidekiq::Web => '/sidekiq'
-=======
 Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   root 'pages#home'
   mount Sidekiq::Web => '/sidekiq'
   resources :jobs
-
->>>>>>> facb7862b40baff8483352c083dc17b464d4b255
 end
