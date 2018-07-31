e5329d67d4a78f6d2828d3006781b3b1c3013455
bercht <bercht@live.com>
2018-07-30 22:20:29 -0300

    models, esboço da index/jobs, controller inicial jobs

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   root 'pages#home'
+  resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
