2db0788a8499489c83ea40aba8ad631e9c53fdc9
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-31 00:29:13 -0300

    change method named call to crawlerEmpregos and its return to list of list in app/services/crawler_service_empregos.rb and create the second test in spec/services/empregos_spec.rb wich expect get a list of list with the first and second jobs of page

--- app/services/crawler_service_empregos.rb
+++ app/services/crawler_service_empregos.rb
@@ -3,11 +3,11 @@ class CrawlerServiceEmprego
         @page = page
     end
 
-    def call
-        return ["Desenvolvedor Ruby", 
+    def crawlerEmpregos
+        return [["Desenvolvedor Ruby", 
         "Atuará no desenvolvimento e deploy de aplicações web AWS e arquitetura de sistemas com base em Ruby on Rails.", 
         "20/07/2018", 
         "Grupo Método em TI",
-        "São Paulo - SP"]
+        "São Paulo - SP"]]
     end
 end
--- spec/services/empregos_spec.rb
+++ spec/services/empregos_spec.rb
@@ -1,16 +1,34 @@
 require 'rails_helper'
 require 'crawler_service_empregos'
 
-describe "crawler_service_empregos_test" do
-    it "service test of empregos.com.br with query 'programador ruby'" do
+describe "crawler_service_empregos to empregos.com.br with query 'programador ruby'" do
+    it "crawlerEmpregos returning the first job in page" do
         @page = get_file_as_string("spec/mocks/Empregos.html")
         #The variable page have the html content from Empregos.com.br 
         #from query 'programador ruby'
-        @results = CrawlerServiceEmprego.new(@campaign).call
-        expect(@results).to eq(["Desenvolvedor Ruby", 
+        @results = CrawlerServiceEmprego.new(@campaign).crawlerEmpregos
+        expect(@results).to eq([["Desenvolvedor Ruby", 
         "Atuará no desenvolvimento e deploy de aplicações web AWS e arquitetura de sistemas com base em Ruby on Rails.", 
         "20/07/2018", 
         "Grupo Método em TI",
-        "São Paulo - SP" ])
+        "São Paulo - SP" ]])
+    end
+
+    it "crawlerEmpregos returning the first and second job in page" do
+        @page = get_file_as_string("spec/mocks/Empregos.html")
+        #The variable page have the html content from Empregos.com.br 
+        #from query 'programador ruby'
+        @results = CrawlerServiceEmprego.new(@campaign).crawlerEmpregos
+        expect(@results).to eq(
+        [["Desenvolvedor Ruby", 
+        "Atuará no desenvolvimento e deploy de aplicações web AWS e arquitetura de sistemas com base em Ruby on Rails.", 
+        "20/07/2018", 
+        "Grupo Método em TI",
+        "São Paulo - SP" ], 
+        ["Analista desenvolvedor especialista Ruby ", 
+        "responsável por projetar, construir e manter um código Ruby testável, eficiente, reutilizável e confiável.", 
+        "19/07/2018", 
+        "Rakuten",
+        "São Paulo - SP" ]])
     end
 end
