3e6ba2355418f640cf8ca78484ae99ddfab87a48
bercht <bercht@live.com>
2018-08-01 21:18:46 -0300

    ajustes no DB, e views

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,11 @@ require 'ffaker'
 
 30.times do
 	Job.create!(
-		job_title: FFaker::Job.title,  
-    	job_description: FFaker::Lorem.paragraph,
+		title: FFaker::Job.title,  
+    	description: FFaker::Lorem.paragraph,
     	published_at: "30/07/2018",
-		company_name: FFaker::Company.name,
-		locale: FFaker::AddressBR.city,
+		company: FFaker::Company.name,
+		location: FFaker::AddressBR.city,
+		url: FFaker::Internet.http_url,
 		)
 end
