0bcd5518870dd88ccffbadf9987b6b77cb88dec0
Thiago-Cardoso <trovador182@gmail.com>
2018-08-03 20:26:24 -0300

    save crawler_profissionais

--- app/services/crawler_profissionais_service.rb
+++ app/services/crawler_profissionais_service.rb
@@ -5,24 +5,26 @@ class CrawlerProfissionaisService
 
         def call
             parsed_page = Nokogiri::HTML(@page)
-            jobs = Array.new
+           # jobs = Array.new
                 job_listings = parsed_page.css('div.job-list-content')
                 job_listings.each do |job_listing|
-                    job = {
+                    Job.create!(
                         title: job_listing.css('h4').text,
                         company: job_listing.css('a')[1].text,
                         location: job_listing.css('a')[2].text,
                         published_at: job_listing.css('a')[4].text,
                         description:  job_listing.css('a')[3].text,
-                        url:  job_listing.css('a')[0].attributes["href"].value
-                    }
+                        url:  job_listing.css('a')[0].attributes["href"].value)
+                end  
+
+
                     #byebug
         
-                job[:url] = job.values[5]
+                #job[:url] = job.values[5]
         
-                jobs << job
-                end
-                return jobs.take(2)
-        byebug
+                #jobs << job
+               # end
+                #return jobs.take(2)
+        #byebug
     end
 end
